912fda69c4f9d11a36e77fc9aa7808a13bc8805b
Brian Ketelsen <bketelsen@gmail.com>
2022-08-18 20:51:22 -0400

    first attempt at build

--- home-manager/darwin/home.nix
+++ home-manager/darwin/home.nix
@@ -20,13 +20,54 @@
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
-
+    home.packages = with pkgs; [
+        age
+        cachix
+        comma
+        curl
+        fd
+        ffmpeg
+        gawk
+        git
+        gnugrep
+        gnupg
+        gnused
+        google-cloud-sdk
+        helmfile
+        htop
+        httpie
+        jq
+        kubectl
+        kubernetes-helm
+        luajit
+        mmv
+        neovim
+        nix
+        nixfmt
+        nixpkgs-fmt
+        nodejs_latest
+        openssh
+        pandoc
+        parallel
+        pkgs.coreutils-full
+        pre-commit
+        neofetch
+        nix-prefetch
+        rclone
+        ripgrep
+        rsync
+        shellcheck
+        stylua
+        sysdo
+        tealdeer
+        terraform
+        treefmt
+        yarn
+        yq-go
+      ];
   # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git.enable = true;
-
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "22.05";
